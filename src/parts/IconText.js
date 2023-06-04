15bb0dcd200e315b3ca221a9ccaf05d43c62181c
taufiqhida <56679957+taufiqhida@users.noreply.github.com>
2023-06-05 06:38:55 +0700

    hero

--- src/parts/IconText.js
+++ src/parts/IconText.js
@@ -5,7 +5,7 @@ import Button from 'elements/Button';
 export default function IconText() {
   return (
     <Button className="bran-text-icon" href="" type="link">
-        Myperfect <span className='text-gray-900'>Hotel.</span>
+        myPerfect <span className='text-gray-900'>Hotel.</span>
     </Button>
   )
 }
