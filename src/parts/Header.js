d4d7114c101226ed25a6df4ed1de3e04cdd825d0
taufiqhida <56679957+taufiqhida@users.noreply.github.com>
2023-06-04 11:28:57 +0700

    Header

---
+++ src/parts/Header.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import Button from 'elements/Button'
+import BranIcon from 'parts/IconText'
+import { useLocation } from 'react-router-dom'
+export default function Header() {
+    const location = useLocation();
+    const getNavLinkClass = (path) => {
+      return location.pathname === path ? "active" : ""
+    }
+  return (
+    <header className = "spacing-sm">
+        <div className='container'>
+            <nav className='navbar navbar-expand-lg navbar-light'>
+                <BranIcon />
+            <div className='collapse navbar-collapse'>
+            <ul className='navbar-nav ml-auto'>
+                <li className={`nav-item${getNavLinkClass("/")}`}>
+                    <Button className="nav-link" type="link" href="">
+                        Home
+                    </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
+                    <Button className="nav-link" type="link" href="/browse-by">
+                        Browse-by
+                    </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/stories")}`}>
+                    <Button className="nav-link" type="link" href="/stories">
+                        Stories
+                    </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/agent")}`}>
+                    <Button className="nav-link" type="link" href="/agent">
+                        Agent
+                    </Button>
+                </li>
+            </ul>
+            </div>
+            </nav>
+        </div>
+    </header>
+  )
+}
