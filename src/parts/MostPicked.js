5faece5345c1a7e989a3c6438e1aa590d3ac6af0
taufiqhida <56679957+taufiqhida@users.noreply.github.com>
2023-06-07 17:35:31 +0700

    finish commponent mostpicked and categories

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -4,7 +4,7 @@ import Button from 'elements/Button';
 
 export default function MostPicked(props) {
   return (
-    <section className='container' ref={props.refMostPicked} style={{height: 2000}}>
+    <section className='container' ref={props.refMostPicked} >
         <h4 className='mb-3'>MostPicked</h4>
         <div className='container-grid'>
             {props.data.map((item, index)=>{
